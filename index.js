cd4165bd7a191aa20d11e66ae4c176d7475a6aad
Lucas Reppe Welander <lrwelander@upsales.com>
2016-11-25 22:55:58 +0100

    minor hotfix

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const _ = require('lodash');
 const chalk = require('chalk');
-const request = require('request');
 const axios = require('axios');
-const async = require('async');
 const API = 'http://api.forismatic.com/api/1.0/';
 
 module.exports = (cb) => {
     axios.get(`${API}?method=getQuote&format=json&lang=en`).then((response) => {
-        cb(
-            console.log(chalk.bold.red(`${response.data.quoteText}`) + chalk.bold.blue.italic(`\n-- ${response.data.quoteAuthor}`))
-        );
+        cb(console.log(chalk.bold.red(`${response.data.quoteText}`) + chalk.bold.blue.italic(`\n-- ${response.data.quoteAuthor}`)));
     });
 }
